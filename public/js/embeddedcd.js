46f47c5294e8536de8d99509fdadb9ee2bd74675
MiqeG <mghilescu@protonmail.com>
2019-04-16 00:40:37 +0200

    fixed api bugs2

--- public/js/embeddedcd.js
+++ public/js/embeddedcd.js
@@ -1,29 +1,32 @@
 
 $('.ui.form.countdown').addClass('loading')
-let date=$('#userDateDiv').text() // get date from DB not implemented
+let date = $('#userDateDiv').text() // get date from DB not implemented
 let interval;
-$.ajax({ 
-    url: "https://2gl2bug4fj.execute-api.eu-west-3.amazonaws.com/latest/countdown?date="+date,
+$.ajax({
+    url: "https://2gl2bug4fj.execute-api.eu-west-3.amazonaws.com/latest/countdown?date=" + date,
     type: "get", //send it through get method
     data: {
 
     },
     success: function (result) {
         //Do Something
-        
-        
+
+
         $('#hiddenDiv').text(result.remainingTime)
         var counter = result.remainingTime;
         newCountDown(result.calculusDate)
+        let string=result.prestring
+        
         function newCountDown(date) {
 
-           
-            
+          
+
             var countDownDate = new Date(date).getTime();
-           
+            
 
             // Update the count down every 1 second
             var x = setInterval(function () {
+               
                 intrerval = x
                 // Get todays date and time
                 var now = new Date().getTime();
@@ -32,24 +35,26 @@ $.ajax({
                 var distance = countDownDate - now;
 
                 // Time calculations for days, hours, minutes and seconds
-                var days= Math.floor((distance % (1000 * 60 * 60 * 24*365)) / (1000 * 60 * 60*24));
-                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+                    let finalstring= string +moment(now).countdown(countDownDate).toString()
+                     // If the count down is 0, write some text 
+                    if (distance==0){
+                        finalstring="DONE!!!"
+                    }
                 // Output the result in an element with id="demo"
-                document.getElementById("countDiv").innerHTML = days+" jours "+ hours+" heures "+
-                    minutes + " minutes et " + seconds + "secondes ";
-                    $("#DateDiv").text(result.countDownDateFormatted);
+                document.getElementById("countDiv").innerHTML = finalstring
+                $("#DateDiv").text(result.countDownDateFormatted);
+              
                     $('.fakeloader').fadeIn(600)
                     $('.ui.form.countdown').removeClass('loading')
-                // If the count down is over, write some text 
-                   
+                
+              
+               
 
-                if (distance < 0) {
-                    document.getElementById("countDiv").innerHTML = "It's time to get noisy champs!!!";
-                    clearInterval(x);
-                   
+
+                
+                if(distance<0){
+                    string='ELAPSED TIME: '
                 }
             }, 1000);
         }
--- server.js
+++ server.js
@@ -1,51 +1,56 @@
 
 
-let express=require('express')
-let app=express()
-let bodyParser=require('body-parser')
+let express = require('express')
+let app = express()
+let bodyParser = require('body-parser')
 let countdown = require('moment-countdown');
-let moment=require('moment')
+let moment = require('moment')
 
 app.use(bodyParser.json());
 app.set('view engine', 'ejs')
 app.use('/assets', express.static('public'))
-app.get('/start',(req,res)=>{
-  if(req.query.date){
-    res.render('pages/embedded', { date:req.query.date})
+app.get('/start', (req, res) => {
+  if (req.query.date) {
+    res.render('pages/embedded', { date: req.query.date })
   }
 })
 
-app.get('/schedule',(req,res)=>{
+app.get('/schedule', (req, res) => {
   res.render('pages/schedule')
 })
 app.get('/countdown', (req, res) => {
-  let locale='fr'
-  if(req.query.locale){
-    locale=req.query.locale
+  let locale = 'en'
+  if (req.query.locale) {
+    locale = req.query.locale
   }
-  moment.locale(locale); 
-  if(req.query.date){
-    let calculusDate=new Date(req.query.date)
-    calculusDate=calculusDate-(2*60*60*1000)
-    let countDownDate = moment(req.query.date).format()
-   
+  moment.locale(locale);
+  if (req.query.date) {
+    let calculusDate = new Date(req.query.date)
+    calculusDate = calculusDate - (2 * 60 * 60 * 1000)
     let dateNow = new Date
+    let countDownDate = moment(req.query.date).format()
+    let configString = 'REMAINING TIME: '
+    if (calculusDate < dateNow) {
+      configString = 'ELAPSED TIME: '
+    }
+ 
     let finalDate = countDownDate - dateNow
     var duration = moment(dateNow).countdown(countDownDate).toString()
     let json = {
-      calculusDate:calculusDate,
+      calculusDate: calculusDate,
       countDownDate: countDownDate,
       dateNow: dateNow,
       remainingTime: finalDate,
       dateNowFormatted: moment(dateNow).format('Do MMMM YYYY, h:mm:ss'),
       countDownDateFormatted: moment(countDownDate).format('Do MMMM YYYY, h:mm:ss'),
-      remainingFormatted: duration
-  
+      remainingFormatted: duration,
+      prestring: configString
+
     }
     res.json(json)
-  }else{
-    res.json({"error":"no date provided"})
+  } else {
+    res.json({ "error": "no date provided" })
   }
-   
-  })
-  module.exports=app
+
+})
+module.exports = app
